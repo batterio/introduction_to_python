3375dd8da156c560b12561d9301cb0ae2daf9946
batterio <loris.mularoni@gmail.com>
2025-02-21 12:08:43 +0100

    deleted old files

--- 2023/boot/open_jupyterlab.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-cd /home/master_ub/master_ub/course
-/home/master_ub/.local/bin/jupyter-lab
--- 2024/boot/open_jupyterlab.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-cd /home/masterub/master_ub/course
-/home/masterub/.local/bin/jupyter-lab
